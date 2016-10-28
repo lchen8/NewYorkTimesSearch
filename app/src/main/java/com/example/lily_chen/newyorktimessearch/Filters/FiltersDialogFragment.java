f70fe804b6d53d101de5af925c2cf9bc776498b4
Lily Chen <lchen8@wellesley.edu>
2016-10-28 12:38:33 -0700

    things are working yayy

--- app/src/main/java/com/example/lily_chen/newyorktimessearch/Filters/FiltersDialogFragment.java
+++ app/src/main/java/com/example/lily_chen/newyorktimessearch/Filters/FiltersDialogFragment.java
@@ -5,7 +5,6 @@ import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.DialogFragment;
 import android.support.v4.app.FragmentManager;
-import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -124,7 +123,6 @@ public class FiltersDialogFragment extends DialogFragment implements DatePickerD
         c.set(Calendar.DAY_OF_MONTH, dayOfMonth);
         beginDate = new Date(year, monthOfYear, dayOfMonth);
         tvDate.setText(String.format("%d / %d / %d", monthOfYear+1, dayOfMonth, year));
-        Log.i("Debug", tvDate.getText().toString());
     }
 
     @OnClick(R.id.btSave)
